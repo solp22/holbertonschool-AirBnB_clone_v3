c0c770e9c4155c1f226ab5459a4d53e13cd00fd8
solp22 <sol.puentereal@gmail.com>
2023-08-23 09:52:32 -0700

    handle 404 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """start the api"""
-from flask import Flask
+from flask import Flask, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -12,6 +12,13 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def close_app(exception):
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """return error"""
+    return make_response({"error": "Not found"}, 404)
+
 
 
 if __name__ == "__main__":
