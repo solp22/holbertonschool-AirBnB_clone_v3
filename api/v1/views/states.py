175739166a7ecc1b252b5f6726a51d37ce31e2c9
Aldo2303 <aldod03@gmail.com>
2023-08-23 19:06:45 -0700

    add create and update method to state route

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """handle RESTful API in state"""
 from models.state import State
-from flask import abort
+from flask import abort, jsonify, request
 from api.v1.views import app_views
 from models import storage
 
@@ -31,3 +31,36 @@ def delete_state(state_id):
     storage.delete(states)
     storage.save()
     return {}, 200
+
+@app_views.route('/states', methods=['POST'])
+def create_state():
+    """Creates a State: POST /api/v1/states"""
+    object_data = request.get_json()
+
+    if not object_data:
+        abort(400, "Not a JSON")
+
+    if "name" not in object_data:
+        abort(400, "Missing name")
+    
+    obj = State(**object_data)
+    storage.new(obj)
+    storage.save()
+    return jsonify(obj.to_dict()), 201
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def update_state(state_id):
+    """Returns the State object with the status code 200"""
+    obj = storage.get(State, state_id)
+    if obj is None:
+        abort(404)
+
+    obj_data = request.get_json()
+    if not obj_data:
+        abort(400, "Not a JSON")
+
+    for key, value in obj_data.items():
+        if key != 'id' and key != 'created_at' and key != 'updated_at':
+            setattr(obj, key, value)
+    storage.save()
+    return jsonify(obj.to_dict()), 200
